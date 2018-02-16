eecb86f402dab1209ef29c102a5a8c271947ba93
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-16 17:57:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nestedTarget(){
 function increaseRankBy(n){
   var ranks = document.querySelectorAll('ul.ranked-list li');
   for(let i = 0; i < ranks.length; i++){
-    
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
   }
 }
